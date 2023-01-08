f033838fd43cefa9ab1ec05399cd40622cf4ec63
alishkeir <ali.shkeir@itxi.net>
2023-01-08 13:52:07 +0200

    Created CategoryItem component

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import CategoryItem from './src/components/CategoryItem';
 import Header from './src/components/Header';
 import Search from './src/components/Search';
 
@@ -8,6 +9,7 @@ const App = () => {
         <View style={styles.container}>
             <Header />
             <Search />
+            <CategoryItem />
             <StatusBar />
         </View>
     );
